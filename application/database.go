73bb93e77370660f041002e194826cb312ea6997
JF-hearX <jannes.fleming@hearxgroup.com>
2025-07-12 17:56:44 +0200

    Updated connection string code

--- application/database.go
+++ application/database.go
@@ -3,6 +3,7 @@ package application
 import (
 	"context"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -12,11 +13,11 @@ import (
 const driverName = "mysql"
 
 func DatabaseProvider(lc fx.Lifecycle) (*sqlx.DB, error) {
-	// url, ok := os.LookupEnv("MYSQL_DSN")
-	// if !ok {
-	// 	return nil, fmt.Errorf("MYSQL_DSN environment variable not set")
-	// }
-	connectionString := "todo:eRwFeXTtrvYAQnKifDeGcN7M@tcp(127.0.0.1:3306)/todo?multiStatements=true&sql_mode=TRADITIONAL&timeout=5s"
+	connectionString, ok := os.LookupEnv("MYSQL_DSN")
+	if !ok {
+		return nil, fmt.Errorf("MYSQL_DSN environment variable not set")
+	}
+
 	var err error
 	db, err := sqlx.Connect(driverName, connectionString)
 	if err != nil {
